e9ceb277032e8177ca9a9091b1b164c06e5388c9
Kshitij Dhyani <wimpywarlord@kshitijs-air.dhcp.wustl.edu>
2024-08-09 09:28:05 -0500

    move FE hosting from github pages into render

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/GradBro/', // Make sure this matches your repository name
+  base: '/', // Make sure this matches your repository name
   plugins: [react()],
 });
